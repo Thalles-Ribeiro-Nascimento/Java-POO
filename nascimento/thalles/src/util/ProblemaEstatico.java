262e34c1d76350b7b79dbc01b3165a7887fbf615
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2025-06-12 19:06:03 -0300

    Refatorando

--- nascimento/thalles/src/util/ProblemaEstatico.java
+++ nascimento/thalles/src/util/ProblemaEstatico.java
@@ -7,7 +7,24 @@ public class ProblemaEstatico {
         return 2 * PI * raio;
     }
 
-    public static double calculoVolume (double raio){
+    public static double calculoVolume(double raio){
         return (4.0 / 3) * PI * Math.pow(raio, 3);
+    }
+
+    public static double calculoAreaTriangule(double a, double b, double c){
+        double p = (a + b + c)/ 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public static double areaRetangle(double base, double height){
+        return height * base;
+    }
+
+    public static double perimeterRetangle(double base, double height){
+        return 2 * (base + height);
+    }
+
+    public static double diagonalRetangle(double base, double height){
+        return Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
     }
 }
